741b9f6bcce765555a13a38f5ba48d34e083e888
Qratto <dskarupin28@gmail.com>
2026-04-21 18:59:26 +0700

    creating structure app

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello world")
+print("Приложение: Todo List")
+
+tasks = {}
+
+while True:
+    print("1 - Просмотр задач \n"
+          "2 - Создание задачи  \n"
+          "3 - Изменение задачи \n"
+          "4 - Удаление задачи \n"
+          "5 - Выход из приложения \n"
+          "Выберите действие ", end="")
+
+    action = int(input())
+
+    if action == 1:
+        pass
+    elif action == 2:
+        pass
+    elif action == 3:
+        pass
+    elif action == 4:
+        pass
+    elif action == 5:
+        break
+    else:
+        print("Неправильный ввод")

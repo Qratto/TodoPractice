7d66650be7fce3d366e9c1434271802be96cc6b9
Qratto <dskarupin28@gmail.com>
2026-04-23 14:32:47 +0700

    creating functionality 3

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def indentation_symbol(func):
+    def wrapper(task_id, task_value):
+        print("======== \n" + func(task_id, task_value)  + "\n" + "======== \n")
+        return None
+    return wrapper
+
+@indentation_symbol
+def update_task(task_id, task_value):
+    global tasks
+    if not tasks:
+        return "Задач нет"
+    if not task_id in list(tasks.keys()):
+        return "Такой задачи нет"
+    tasks[task_id] = task_value
+    return "Задача обновлена"
+
 print("Приложение: Todo List")
 
 tasks = {}
@@ -17,7 +33,11 @@ while True:
     elif action == 2:
         pass
     elif action == 3:
-        pass
+        print("Введите номер задачи: ", end="")
+        task_id = int(input())
+        print("Введите Еновую задачу: ", end="")
+        task_value = input()
+        update_task(task_id, task_value)
     elif action == 4:
         pass
     elif action == 5:
